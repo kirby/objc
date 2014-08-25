92928c2ea5fe6fedf1bf3622f32cfae968fd15b3
Kirby Shabaga <kirby.shabaga@gmail.com>
2014-08-25 16:32:52 -0700

    update gitignore

---
+++ ObjectiveCDemo/ObjectiveCDemo/GitHubMyFollowers.m
@@ -0,0 +1,13 @@
+//
+//  GitHubMyFollowers.m
+//  ObjectiveCDemo
+//
+//  Created by Kirby Shabaga on 8/25/14.
+//  Copyright (c) 2014 Worxly. All rights reserved.
+//
+
+#import "GitHubMyFollowers.h"
+
+@implementation GitHubMyFollowers
+
+@end
